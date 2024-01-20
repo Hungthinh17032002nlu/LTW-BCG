b1dec80e8778dcf8874e8e746b3df952b4592fdf
Hungthinh17032002nlu <20130123@st.hcmuaf.edu.vn>
2024-01-20 15:55:35 +0700

    thinh comit

---
+++ untitled/src/main/webapp/Login/login.js
@@ -0,0 +1,25 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert(`Welcome, ${data.username}!`);
+                window.location.href = '/dashboard';
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
+        });
+}
